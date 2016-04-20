cb34d3a73a8f570f52b48967edf659ced1ece0a8
safooray <safoora.yousefi1192@gmail.com>
2016-04-20 07:26:54 -0400

    Update BuildSimGraph.java

--- ir-project/src/main/java/BuildSimGraph.java
+++ ir-project/src/main/java/BuildSimGraph.java
@@ -11,15 +11,18 @@ public class BuildSimGraph {
 		WordGraph wg = new WordGraph();
 		//adding nodes 
 		for (int n = 0; n < GraphWords.size(); n++){
-			wg.addVertex(GraphWords.get(n));
+			if (util.vec.hasWord(GraphWords.get(n))) wg.addVertex(GraphWords.get(n));
 		}
 		for (int n = 0; n < GraphWords.size()-1; n++) {
 			for (int m = n+1; m < GraphWords.size(); m++) {
 			
 					String w1 = GraphWords.get(n);
 					String w2 = GraphWords.get(m);
-					double dist = util.distance(w1, w2);
-					wg.addWeightedEdge(w1, w2, dist);
+					if(wg.getWordGraph().containsVertex(w1) && wg.getWordGraph().containsVertex(w2))
+					{
+						double dist = util.distance(w1, w2);
+						wg.addWeightedEdge(w1, w2, dist);
+					}
 			}
 		}
 				
@@ -31,7 +34,7 @@ public class BuildSimGraph {
 		wg.centralityAnalysis(wg.getMST(),"weighted");
     	wg.filterTopics(-1.0);    			
 		fw.flush();	
-		fw.write("MST weighted ");
+		fw.write("MST weighted");
 		fw.write("\n"+wg.topicToString()+"\n");
 		
 		wg.centralityAnalysis(wg.getMST(),"closeness");
@@ -49,20 +52,20 @@ public class BuildSimGraph {
 		wg.centralityAnalysis(wg.getMST(),"betweenness");
 		wg.filterTopics(-1.0);    			
 		fw.flush();	
-		fw.write("MST betwennness");
+		fw.write("MST betwenness");
 		fw.write("\n"+wg.topicToString()+"\n");
 		
 		
 		wg.centralityAnalysis(wg.getWordGraph(),"weighted");
     	wg.filterTopics(-1.0);    			
 		fw.flush();	
-		fw.write(" weighted ");
+		fw.write("WG weighted");
 		fw.write("\n"+wg.topicToString()+"\n");
 		
 		wg.centralityAnalysis(wg.getWordGraph(),"closeness");
 		wg.filterTopics(-1.0);    			
 		fw.flush();	
-		fw.write(" closeness");
+		fw.write("WG closeness");
 		fw.write("\n"+wg.topicToString()+"\n");
 				
 		fw.close();
